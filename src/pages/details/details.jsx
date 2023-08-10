ad0b43b0620b9779eb5f5ca1d6b9a24de879e546
Akpa Onyedikachukwu <akpa3220@gmail.com>
2023-08-10 21:31:32 +0100

    new

--- src/pages/details/details.jsx
+++ src/pages/details/details.jsx
@@ -1,29 +1,29 @@
-import axios from "axios";
+// import axios from "axios";
 import { IoIosCloseCircle } from "react-icons/io";
 
 function Details({ close, id }) {
-  const movieDetails = () => {
-    const options = {
-      method: "GET",
-      url: `https://api.themoviedb.org/3/movie/${id}`,
-      params: { language: "en-US" },
-      headers: {
-        accept: "application/json",
-        Authorization: import.meta.env.VITE_AUTH,
-      },
-    };
+  // const movieDetails = () => {
+  //   const options = {
+  //     method: "GET",
+  //     url: `https://api.themoviedb.org/3/movie/${id}`,
+  //     params: { language: "en-US" },
+  //     headers: {
+  //       accept: "application/json",
+  //       Authorization: import.meta.env.VITE_AUTH,
+  //     },
+  //   };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response);
-        console.log(id);
-      })
-      .catch(function (error) {
-        console.error(error.response);
-        console.log(id);
-      });
-  };
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       console.log(response);
+  //       console.log(id);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error.response);
+  //       console.log(id);
+  //     });
+  // };
 
   return (
     <div className='absolute left-0 right-0 top-0 z-20 mx-auto min-h-screen overflow-hidden rounded-md border-[1px] border-sub bg-primaryDark lg:top-5 lg:w-[90%]'>
@@ -33,7 +33,7 @@ function Details({ close, id }) {
         </button>
       </div>
 
-      
+
     </div>
   );
 }
